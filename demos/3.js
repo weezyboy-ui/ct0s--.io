8025c976f2531f06a47d3bc37339d621735a82ef
weezyboi-ui <kyleperaltahax@gmail.com>
2023-12-18 21:23:17 +0800

    samplecom

---
+++ demos/3.js
@@ -0,0 +1,6 @@
+<script>
+setInterval(function() {
+var glitchText2 = document.querySelector('.question');
+glitchText2.innerText = glitchText2.innerText === 'SO ARE YOU READY?' ? '?!?!?READY!!??!' : 'SO ARE YOU READY?' ;
+}, 1000);
+</script>
